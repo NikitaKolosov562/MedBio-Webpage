ae5ae895f6a62b68b642b1005ee5d2209b53bcdc
NikitaKolosov562 <nkolosov08@icloud.com>
2024-09-25 13:14:26 -0700

    fixed error

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 
-  $("#answers").click(function() {
+$("#answers").click(function() 
+{ $(".answers").show() }); 
 
-    $(".answers").show();
-  });
+$("#answers2").click(function() 
+{ $(".answers2").show() }); 
+
+$("#answers3").click(function() 
+{ $(".answers3").show() }); 
+
+
+
 });
+
+    
+	
+	
+  
+
